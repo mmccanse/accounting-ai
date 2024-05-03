bc44c9a4ab165318e5a5cebe5f0d151db8063c3d
mmccanse <148015402+mmccanse@users.noreply.github.com>
2024-05-03 00:56:03 -0600

    update instructions with line break

--- Video_Assistant.py
+++ Video_Assistant.py
@@ -193,7 +193,8 @@ def main():
     </style>
     """, unsafe_allow_html=True)
     
-    question = st.text_input('Ask me questions about the video! For instance: <br>Give me a bulleted list of the main talking points and a summary of each.<br>Write step by step instructions for how to do the process in the video.')
+    question = st.text_input("""Ask me questions about the video! For instance: <br>Give me a bulleted list of the main talking 
+                             points and a summary of each.<br>Write step by step instructions for how to do the process in the video.""",, unsafe_allow_html=True)
     submit_question = question_button_and_style()
     
     st.markdown(f"**Example questions to try:**")

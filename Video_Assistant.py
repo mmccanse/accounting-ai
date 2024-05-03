ee1dcba25fbd571bb59dc6edb28e8b86134b9405
mmccanse <148015402+mmccanse@users.noreply.github.com>
2024-05-03 01:05:05 -0600

    move instructions

--- Video_Assistant.py
+++ Video_Assistant.py
@@ -197,7 +197,7 @@ def main():
     st.write("Give me a bulleted list of the main talking points and a summary of each.")
     st.write("Write step by step instructions for how to do the process in the video.")
     
-    question = st.text_input("Ask me questions about the video! For instance: Give me a bulleted list of the main talking points and a summary of each.<br>Write step by step instructions for how to do the process in the video.")
+    question = st.text_input("Ask me questions about the video!")
     submit_question = question_button_and_style()
     
 
